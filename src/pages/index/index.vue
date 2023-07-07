e8bd31a6c9458cf0c1562e7e8b74db066aa06d05
yang1206 <yang15188280673@outlook.com>
2023-07-08 00:03:56 +0800

    feat: 修改部分样式

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,18 +3,17 @@
     <div>
       <div class="i-carbon-campsite" inline-block text-80 />
       <p>
-        <a rel="noreferrer" href="https://github.com/yang1206/uniapp-template" target="_blank">
+        <a text-sm text-gray decoration-none op75 rel="noreferrer" href="https://github.com/yang1206/uniapp-template" target="_blank">
           一个uniapp，vite，vue3的起始模版
         </a>
-      </p>
-      <p>
-        <em class="text-sm op75">Opinionated Vite Starter Template</em>
       </p>
       <div class="py-4" />
       <Counter />
     </div>
-    <div text-cyan class="content">
+    <div flex="~ justify-evenly">
       <image class="logo" src="/static/logo.png" />
+      <image class="logo" src="/static/vue.svg" />
+      <image class="logo" src="/static/vite.svg" />
     </div>
   </div>
 </template>
@@ -29,28 +28,11 @@
 </route>
 
 <style scoped>
-.content {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-
 .logo {
-  margin-left: auto;
-  margin-right: auto;
   margin-top: 200rpx;
   margin-bottom: 50rpx;
-  width: 200rpx;
-  height: 200rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
+  width: 100rpx;
+  height: 100rpx;
+  text-decoration: none;
 }
 </style>
